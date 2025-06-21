96ccd0288958035b349cd0d29e5340869cb8564b
saurav-GLA <saurav.rai@gla.ac.in>
2025-06-21 16:45:11 +0530

    changed bookmyshow to bookmyseat

--- bookymyshow/wsgi.py
+++ bookymyshow/wsgi.py
@@ -1,11 +1,4 @@
-"""
-WSGI config for bookymyshow project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
 
 import os
 
@@ -14,3 +7,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookymyshow.settings')
 
 application = get_wsgi_application()
+app = application
