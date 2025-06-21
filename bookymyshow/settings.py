204b19db0e494d9c202e8c4af68cb19d644b5c80
saurav-GLA <saurav.rai@gla.ac.in>
2025-06-21 16:31:50 +0530

    commit commit

--- bookymyshow/settings.py
+++ bookymyshow/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +80,6 @@ WSGI_APPLICATION = 'bookymyshow.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
